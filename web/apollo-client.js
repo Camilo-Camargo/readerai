f1f18680f375adb1d24e0842df8a44652ef4d96e
camilo-camargo <camilocamargo49@gmail.com>
2023-04-26 12:40:47 -0500

    Change src to app directory Nextjs 13 beta

--- web/apollo-client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const ApolloClientPy = new ApolloClient({
-  uri: "http://0.0.0.0:8000/graphql",
-  cache: new InMemoryCache(),
-});
-
-export default ApolloClientPy;
--- web/next.config.js
+++ web/next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites() {
-    return [
-      {
-        source: "/graphql",
-        destination: "http://0.0.0.0:8000/graphql"
-      }
-    ]
-  },
   experimental: {
     appDir: true,
   },
